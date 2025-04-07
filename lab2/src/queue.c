947e1a125c42034ccbb458e43404ee2878d5901e
Sockish <wlm24@mails.tsinghua.edu.cn>
2025-04-07 14:19:03 +0100

    Refactor queue implementation to support blocking behavior and add multi-threading for producer and consumer tasks

--- lab2/src/queue.c
+++ lab2/src/queue.c
@@ -1,43 +1,105 @@
 // naive implementation
 #include "mm.h" // MMData
 #include "queue.h"
+#include <pthread.h>
 
-struct MMQueue{
-    // Circular Queue
-    MMData data[MAX_QUEUE_LEN];
-    // head, tail of this queue
-    int h, t; 
-    // closed means no more data can be added
-    int closed;
-}QData;
+struct MMQueue
+{
+	// Circular Queue
+	MMData data[MAX_QUEUE_LEN];
+	// head, tail of this queue
+	int h, t;
+	// closed means no more data can be added
+	int closed;
+	// Add synchronization primitives
+	pthread_mutex_t mutex;
+	pthread_cond_t not_empty;
+	pthread_cond_t not_full;
+} QData;
 
-void queue_init(){
-    // init/reset the queue
-    QData.h = 0;
-    QData.t = 0;
-    QData.closed = 0;
+void queue_init()
+{
+	// init/reset the queue
+	QData.h = 0;
+	QData.t = 0;
+	QData.closed = 0;
+
+	// Initialize synchronization primitives
+	pthread_mutex_init(&QData.mutex, NULL);
+	pthread_cond_init(&QData.not_empty, NULL);
+	pthread_cond_init(&QData.not_full, NULL);
 }
 
-int queue_get(MMData *ret){
-    // this naive implementation did not support block when queue is empty, just return -1
-    if(QData.h >= QData.t) return -1;
-    int h = QData.h;
-    QData.h++;
-    *ret = QData.data[h%MAX_QUEUE_LEN]; // circular use the QData.data
-    return 0; // 0 means success
+int queue_get(MMData *ret)
+{
+	pthread_mutex_lock(&QData.mutex);
+
+	// Wait if queue is empty and not closed
+	while (QData.h >= QData.t && !QData.closed)
+	{
+		pthread_cond_wait(&QData.not_empty, &QData.mutex);
+	}
+
+	// If queue is empty and closed, return -1
+	if (QData.h >= QData.t)
+	{
+		pthread_mutex_unlock(&QData.mutex);
+		return -1;
+	}
+
+	int h = QData.h;
+	QData.h++;
+	*ret = QData.data[h % MAX_QUEUE_LEN]; // circular use the QData.data
+
+	// Signal that the queue is not full anymore
+	pthread_cond_signal(&QData.not_full);
+	pthread_mutex_unlock(&QData.mutex);
+
+	return 0; // 0 means success
 }
 
-int queue_add(MMData data){
-    printf("MAX LEN = %d", (MAX_QUEUE_LEN));
-    // this naive implementation did not support block when queue is full, just return -1
-    if(QData.t - QData.h >= MAX_QUEUE_LEN) return -1;
-    if(QData.closed) return -1;
-    int t = QData.t;
-    QData.t++;
-    QData.data[t%MAX_QUEUE_LEN] = data;
-    return 0; // 0 means success
+int queue_add(MMData data)
+{
+	pthread_mutex_lock(&QData.mutex);
+
+	// Check if queue is closed
+	if (QData.closed)
+	{
+		pthread_mutex_unlock(&QData.mutex);
+		return -1;
+	}
+
+	// Wait if queue is full
+	while (QData.t - QData.h >= MAX_QUEUE_LEN && !QData.closed)
+	{
+		pthread_cond_wait(&QData.not_full, &QData.mutex);
+		// Check again if queue was closed while waiting
+		if (QData.closed)
+		{
+			pthread_mutex_unlock(&QData.mutex);
+			return -1;
+		}
+	}
+
+	int t = QData.t;
+	QData.t++;
+	QData.data[t % MAX_QUEUE_LEN] = data;
+
+	// Signal that the queue is not empty anymore
+	pthread_cond_signal(&QData.not_empty);
+	pthread_mutex_unlock(&QData.mutex);
+
+	return 0; // 0 means success
 }
 
-void queue_close(){
-    QData.closed = 1;
+void queue_close()
+{
+	pthread_mutex_lock(&QData.mutex);
+	QData.closed = 1;
+
+	// Signal all waiting threads
+	pthread_cond_broadcast(&QData.not_empty);
+	pthread_cond_broadcast(&QData.not_full);
+
+	pthread_mutex_unlock(&QData.mutex);
 }
--- lab2/src/task1.c
+++ lab2/src/task1.c
@@ -6,35 +6,83 @@
 
 #include "task1.h" // TASK1_TASKS_NUM
 
+// Mutex for protecting stdout
+pthread_mutex_t print_mutex;
 
-// naive implementation
-void producer(){
-    int i, res;
-    for(i=0; i<TASK1_TASKS_NUM; i++){
-        MMData data;
-        data.task_id = i;
-        res = queue_add(data);
-        if(res == -1)
-            printf("queue_add error\n"); // this should never happen
-    }
-    queue_close(); // send queue_close()
+// Thread function for producer
+void *producer_thread(void *arg)
+{
+	int i, res;
+	for (i = 0; i < TASK1_TASKS_NUM; i++)
+	{
+		MMData data;
+		data.task_id = i;
+		res = queue_add(data);
+		if (res == -1)
+			printf("queue_add error\n"); // this should never happen
+	}
+	queue_close(); // send queue_close()
+	return NULL;
 }
 
-// naive implementation
-void consumer(){
-    MMData ret;
-    while(queue_get(&ret) == 0){
-        // you may need a mutex lock for output
-        printf("%d\n", ret.task_id);
-    }
+// Thread function for consumer
+void *consumer_thread(void *arg)
+{
+	MMData ret;
+	while (queue_get(&ret) == 0)
+	{
+		// Use mutex lock for output to avoid interleaving
+		pthread_mutex_lock(&print_mutex);
+		printf("%d\n", ret.task_id);
+		pthread_mutex_unlock(&print_mutex);
+	}
+	return NULL;
 }
 
-// you have to modify main to allow multi-threading.
-int main(){
-    queue_init();
-    
-    producer(); // Note: Since there is only one thread, when queue is full, this naive implementation will cause infinitely waiting
-    consumer();
+// You have to modify main to allow multi-threading.
+int main()
+{
+	// Number of consumer threads
+	const int NUM_CONSUMERS = 2;
 
-    return 0;
+	// Initialize the queue
+	queue_init();
+
+	// Initialize the print mutex
+	pthread_mutex_init(&print_mutex, NULL);
+
+	// Create thread handles
+	pthread_t producer;
+	pthread_t consumers[NUM_CONSUMERS];
+
+	// Create the producer thread
+	if (pthread_create(&producer, NULL, producer_thread, NULL) != 0)
+	{
+		fprintf(stderr, "Error creating producer thread\n");
+		return 1;
+	}
+
+	// Create consumer threads
+	for (int i = 0; i < NUM_CONSUMERS; i++)
+	{
+		if (pthread_create(&consumers[i], NULL, consumer_thread, NULL) != 0)
+		{
+			fprintf(stderr, "Error creating consumer thread %d\n", i);
+			return 1;
+		}
+	}
+
+	// Wait for producer to finish
+	pthread_join(producer, NULL);
+
+	// Wait for all consumers to finish
+	for (int i = 0; i < NUM_CONSUMERS; i++)
+	{
+		pthread_join(consumers[i], NULL);
+	}
+
+	// Clean up
+	pthread_mutex_destroy(&print_mutex);
+
+	return 0;
 }
